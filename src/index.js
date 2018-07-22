205952835deafb507f5395a96f9cf4abc4341e51
Lang Gao <langfr.gao@gmail.com>
2018-07-22 01:57:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import App from './container/App';
 
+var http = require("http");
+setInterval(function() {
+    http.get("https://smartbrain-demographics.herokuapp.com/");
+    http.get("https://robogenerator.herokuapp.com/");
+    http.get("https://lemon-music.herokuapp.com/recommend");
+}, 300000); // Pin the heroku apps every 5 minutes
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
  
